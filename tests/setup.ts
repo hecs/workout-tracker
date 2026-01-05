c826b78920084c0698dafac0cbf600c0743281e6
Henrik Friberg <henrik.friberg@gmail.com>
2026-01-05 09:37:21 +0100

    Fix test setup - remove unused cleanup import

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,9 +1,7 @@
 import { expect, afterEach, vi } from 'vitest'
-import { cleanup } from '@testing-library/dom'
 
 // Cleanup after each test
 afterEach(() => {
-  cleanup()
   localStorage.clear()
 })
 
